4d7f762e2bd6c748d89e3e79f37823e450c38f48
mjbuchman <mbookie213@gmail.com>
2022-03-20 17:25:22 -0400

    added header icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Search from "./Search";
 import ResultsTable from "./ResultsTable";
-import { Container, Stack } from 'react-bootstrap';
+import { Stack } from 'react-bootstrap';
+import InfoIcon from '@material-ui/icons/InfoOutlined';
+import AssessmentIcon from '@material-ui/icons/AssessmentOutlined';
 
 function App() {
 	return (
 		<Stack className="App" gap={5}>
-			<div className="header">
+			<Stack className="header" direction="horizontal">
 				<h1 style={{marginLeft: "10px", padding: "0px"}}>HUDDLE</h1>
-			</div>
+				<button className="headerBtn ms-auto"><AssessmentIcon/></button>
+				<button className="headerBtn"><InfoIcon/></button>
+			</Stack>
 			<Search></Search>
 			<ResultsTable></ResultsTable>
 		</Stack>
